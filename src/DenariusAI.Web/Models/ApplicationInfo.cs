d72bcf4c8fa65dca5aa50ff45a00b04bf20bbd0b
ruialexrib <5676648+ruialexrib@users.noreply.github.com>
2026-09-05 19:41:55 +0100

    docs(web): document application information model

--- src/DenariusAI.Web/Models/ApplicationInfo.cs
+++ src/DenariusAI.Web/Models/ApplicationInfo.cs
@@ -1,3 +1,8 @@
 namespace DenariusAI.Web.Models;
 
+/// <summary>
+/// Describes the application version and its user-facing description.
+/// </summary>
+/// <param name="Version">The application version.</param>
+/// <param name="Description">The application description.</param>
 public sealed record ApplicationInfo(string Version, string Description);
