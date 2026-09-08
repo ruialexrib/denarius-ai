ba0e46ca8392329f7abeebb84558cad18410ad07
ruialexrib <5676648+ruialexrib@users.noreply.github.com>
2026-09-08 12:59:00 +0100

    remove legacy analytics view model

--- src/DenariusAI.Web/ViewModels/AnalyticsViewModel.cs
+++
@@ -1,8 +0,0 @@
-using DenariusAI.Application.DTOs;
-using Microsoft.AspNetCore.Mvc.Rendering;
-
-namespace DenariusAI.Web.ViewModels;
-
-public sealed record AnalyticsViewModel(AnalyticsFilterDto Filter, AnalyticsDto Analytics,
-    IReadOnlyList<SelectListItem> Groups, IReadOnlyList<SelectListItem> Categories, IReadOnlyList<SelectListItem> Accounts,
-    IReadOnlyList<DashboardMonthDto> AnnualIncomeExpenses, IReadOnlyList<DashboardBudgetMonthDto> AnnualBudgetExecution);
