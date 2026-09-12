c313ce21771e94e32d034f0701f54cccb4ec870e
ruialexrib <5676648+ruialexrib@users.noreply.github.com>
2026-09-12 23:33:18 +0100

    feat(analytics): register remaining analysis services

--- src/DenariusAI.Application/DependencyInjection.cs
+++ src/DenariusAI.Application/DependencyInjection.cs
@@ -29,6 +29,9 @@ public static class DependencyInjection
         services.AddScoped<IGlobalFinancialViewService, GlobalFinancialViewService>();
         services.AddScoped<IIncomeExpenseFlowAnalysisService, IncomeExpenseFlowAnalysisService>();
         services.AddScoped<IBudgetExecutionAnalysisService, BudgetExecutionAnalysisService>();
+        services.AddScoped<ISavingsLiquidityAnalysisService, SavingsLiquidityAnalysisService>();
+        services.AddScoped<IInvestmentPortfolioAnalysisService, InvestmentPortfolioAnalysisService>();
+        services.AddScoped<IFinancialCommitmentsAnalysisService, FinancialCommitmentsAnalysisService>();
         services.AddScoped<IAssistantService, FinancialAssistantService>();
         services.AddScoped<IJournalEntrySuggestionService, JournalEntrySuggestionService>();
         services.AddScoped<IStockForecastService, StockForecastService>();
